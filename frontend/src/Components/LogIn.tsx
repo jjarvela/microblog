8df9659f7342251acc400564fab6b389e51fac58
Tero Salmela <tero.salmela@reun.eu>
2023-12-15 13:44:33 +0200

    Add button theming example to login route

--- frontend/src/Components/LogIn.tsx
+++ frontend/src/Components/LogIn.tsx
@@ -1,7 +1,12 @@
+import Button from "./Elements/Button";
+
 const LogIn = () => {
-    return(
-        <h2>This is the login page for visitors</h2>
-    );
+  return (
+    <>
+      <h2>This is the login page for visitors</h2>
+      <Button>Log in here!</Button>
+    </>
+  );
 };
 
 export default LogIn;
