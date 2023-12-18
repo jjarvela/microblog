ed28a95916b2a3b67aac64d40c07c5739c8f41f7
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2023-12-18 13:46:17 +0200

    primary button using tailwind classes

--- frontend/src/Components/Elements/Button.tsx
+++ frontend/src/Components/Elements/Button.tsx
@@ -1,6 +1,6 @@
 function Button(props: { children?: React.ReactNode }) {
   return (
-    <button className="from-primaryFrom to-primaryTo font-heading m-4 rounded-full bg-gradient-to-r px-5 py-2 text-lg font-light text-white drop-shadow-md hover:from-[#EF5030] hover:to-[#F27A25]">
+    <button className="btn-primary">
       {props.children}
     </button>
   );
