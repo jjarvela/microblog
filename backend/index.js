5e40551540c3dd12b9726ddf7527495255927407
Joona Jarvela <joona.jarvela@gmail.com>
2023-12-20 08:26:21 +0000

    Server forder restructuring

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
+"use strict";
+const express = require('express');
+const app = express();
+const port = 8080;
 app.get('/', (req, res) => {
-  res.send(`
+    res.send(`
   <!doctype HTML>
   <html>
   <body>
   <div style="margin-top:60px;text-align:center;">The Microblog will be launched here in the spring of 2024. Stay tuned!</div>    
   </body>
-  </html>`)
-})
-
+  </html>`);
+});
 app.listen(port, () => {
-  console.log(`Microblog server listening on port ${port}`)
-})
+    console.log(`Microblog server listening on port ${port}`);
+});
