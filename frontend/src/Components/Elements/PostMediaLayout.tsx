45795a3e74f325afd336c5a426c549aa30baa7dc
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-01-03 18:18:00 +0200

    more styling for media layouts

--- frontend/src/Components/Elements/PostMediaLayout.tsx
+++ frontend/src/Components/Elements/PostMediaLayout.tsx
@@ -14,32 +14,42 @@ export default function PostMediaLayout({ media }: PostMediaProps) {
       case 2:
         return (
           <div className="flex h-[100%] flex-row gap-1">
-            <PostMedia type={items[0].type} source={items[0].source} />
-            <PostMedia type={items[1].type} source={items[1].source} />
+            <div className="flex w-[50%] flex-col">
+              <PostMedia type={items[0].type} source={items[0].source} />
+            </div>
+            <div className="flex w-[50%] flex-col">
+              <PostMedia type={items[1].type} source={items[1].source} />
+            </div>
           </div>
         );
       case 3:
         return (
           <div className="flex h-[100%] gap-1">
-            <PostMedia type={items[0].type} source={items[0].source} />
-            <div className="flex h-[100%] flex-col gap-1">
-              <PostMedia type={items[1].type} source={items[1].source} />
-              <PostMedia type={items[2].type} source={items[2].source} />
+            <div className="flex w-[50%] flex-col">
+              <PostMedia type={items[0].type} source={items[0].source} />
+            </div>
+            <div className="flex w-[50%] flex-col">
+              <div className="mb-0.5 h-[50%]">
+                <PostMedia type={items[1].type} source={items[1].source} />
+              </div>
+              <div className="mt-0.5 h-[50%]">
+                <PostMedia type={items[2].type} source={items[2].source} />
+              </div>
             </div>
           </div>
         );
       case 4:
         return (
           <>
-            <div className="flex h-[50%] flex-row gap-1">
-              <div className="flex w-[50%] flex-col">
+            <div className="mb-1 flex h-[50%] flex-row gap-1">
+              <div className=" flex w-[50%] flex-col">
                 <PostMedia type={items[0].type} source={items[0].source} />
               </div>
               <div className="flex w-[50%] flex-col">
                 <PostMedia type={items[1].type} source={items[1].source} />
               </div>
             </div>
-            <div className="flex h-[50%] flex-row gap-1">
+            <div className="mt-1 flex h-[50%] flex-row gap-1">
               <div className="flex w-[50%] flex-col">
                 <PostMedia type={items[2].type} source={items[2].source} />
               </div>
@@ -55,7 +65,7 @@ export default function PostMediaLayout({ media }: PostMediaProps) {
   };
 
   return (
-    <div className="m-auto h-[25rem] w-[95%] border-spacing-1 border-solid border-black25">
+    <div className=" m-auto h-[25rem] w-[95%] overflow-hidden rounded-lg border-[1px] border-solid border-black25 dark:border-black75">
       {renderLayout(media)}
     </div>
   );
