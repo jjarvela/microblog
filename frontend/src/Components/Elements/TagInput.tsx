06be2b74283a1f949f04f85a4084d3e05a1c88ed
Tero Salmela <tero.salmela@reun.eu>
2024-01-03 09:57:54 +0200

    Move tag add field to end of tag list

--- frontend/src/Components/Elements/TagInput.tsx
+++ frontend/src/Components/Elements/TagInput.tsx
@@ -37,13 +37,6 @@ function TagInput({
   }, [newTag, newTags, maxTagLength, maxTags]);
   return (
     <div className="relative flex min-h-[4rem] w-full max-w-[32rem] resize-none flex-row flex-wrap items-baseline gap-1 rounded-xl border border-black50 bg-white p-2 outline-1 focus-within:border-primary focus-within:shadow-[0px_0px_5px_2px_var()] focus-within:shadow-primary focus-within:outline-none hover:border-black75 dark:bg-black dark:hover:border-black25">
-      <input
-        onChange={(e) => setNewTag(e.currentTarget.value)}
-        placeholder="Add tag..."
-        className="rounded-none border-0 p-0 hover:border-0 focus:border-0 focus:shadow-none focus:outline-none dark:hover:border-0"
-        value={newTag}
-        maxLength={maxTagLength + 1}
-      ></input>
       {newTags.map((val, i) => (
         <TagBox text={val} key={i} onDelete={() => handleTagDelete(i)} />
       ))}
@@ -52,6 +45,13 @@ function TagInput({
           {newTags.length} / {maxTags}
         </p>
       )}
+      <input
+        onChange={(e) => setNewTag(e.currentTarget.value)}
+        placeholder="Add tag..."
+        className="rounded-none border-0 p-0 hover:border-0 focus:border-0 focus:shadow-none focus:outline-none dark:hover:border-0"
+        value={newTag}
+        maxLength={maxTagLength + 1}
+      ></input>
     </div>
   );
 }
