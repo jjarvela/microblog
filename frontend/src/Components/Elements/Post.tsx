9bda82fb7b4bc33bc8a20e2d770b03d441f3a86a
Tero Salmela <tero.salmela@reun.eu>
2024-01-04 10:28:26 +0200

    Add relative parent to Post component

--- frontend/src/Components/Elements/Post.tsx
+++ frontend/src/Components/Elements/Post.tsx
@@ -20,7 +20,7 @@ type PostProps = {
   reactions: number;
   tags: string[];
   time: Date;
-  showContextMenu?: boolean;
+  ownerOptions?: boolean;
 };
 
 function Post({
@@ -34,52 +34,54 @@ function Post({
   media,
   reposter,
   replyingTo,
-  showContextMenu,
+  ownerOptions,
 }: PostProps) {
   return (
-    <div className="timeline-box flex flex-col overflow-clip">
-      {reposter ? (
-        <div className="-mx-3 mb-4 flex flex-row justify-end border-b border-black25 px-6 pb-1 dark:border-white25">
-          <UsernameRepost username={reposter} />
+    <div className="relative">
+      <div className="timeline-box flex flex-col overflow-hidden">
+        {reposter ? (
+          <div className="-mx-3 mb-4 flex flex-row justify-end border-b border-black25 px-6 pb-1 dark:border-white25">
+            <UsernameRepost username={reposter} />
+          </div>
+        ) : null}
+
+        <div className="flex flex-row items-center gap-4">
+          <ProfilePicture width={80} image={profileImage} />
+          <h5>{profileName}</h5>
+          <p className="text-black50">{postOwner}</p>
+          <p className="ml-auto mr-3 self-start">{time.toLocaleString()}</p>
+          <PostContextMenu class="self-start" ownerOptions={ownerOptions} />
         </div>
-      ) : null}
 
-      <div className="flex flex-row items-center gap-4">
-        <ProfilePicture width={80} image={profileImage} />
-        <h5>{profileName}</h5>
-        <p className="text-black50">{postOwner}</p>
-        <p className="ml-auto mr-3 self-start">{time.toLocaleString()}</p>
-        {showContextMenu && <PostContextMenu class="self-start" />}
-      </div>
+        {replyingTo ? (
+          <div className="-mx-3 mt-4 flex flex-row justify-start border-y border-black25 px-4 py-4 dark:border-white25">
+            <InReplyTo username={replyingTo} />
+          </div>
+        ) : null}
 
-      {replyingTo ? (
-        <div className="-mx-3 mt-4 flex flex-row justify-start border-y border-black25 px-4 py-4 dark:border-white25">
-          <InReplyTo username={replyingTo} />
+        <div className="m-6 flex flex-col gap-2">
+          <div>{text}</div>
+
+          {media ? <PostMediaLayout media={media} /> : null}
+
+          <p className="flex flex-row gap-4">
+            {tags.map((val, i) => (
+              <a key={i}>{val}</a>
+            ))}
+          </p>
+          <div className="flex flex-row justify-center gap-4 text-2xl">
+            <MaterialSymbolsFavoriteOutlineRounded />
+            <MaterialSymbolsShareOutline />
+            <MaterialSymbolsChatOutlineRounded />
+            <MaterialSymbolsFlagRounded />
+          </div>
         </div>
-      ) : null}
-
-      <div className="m-6 flex flex-col gap-2">
-        <div>{text}</div>
-
-        {media ? <PostMediaLayout media={media} /> : null}
-
-        <p className="flex flex-row gap-4">
-          {tags.map((val, i) => (
-            <a key={i}>{val}</a>
-          ))}
-        </p>
-        <div className="flex flex-row justify-center gap-4 text-2xl">
-          <MaterialSymbolsFavoriteOutlineRounded />
-          <MaterialSymbolsShareOutline />
-          <MaterialSymbolsChatOutlineRounded />
-          <MaterialSymbolsFlagRounded />
+        <div className="-m-3 flex flex-row items-center justify-end gap-1 bg-black25 px-6 py-3 dark:bg-black75">
+          <span className="text-lg">
+            <PhFireSimpleBold />
+          </span>
+          <p>{reactions} Reactions</p>
         </div>
-      </div>
-      <div className="-m-3 flex flex-row items-center justify-end gap-1 bg-black25 px-6 py-3 dark:bg-black75">
-        <span className="text-lg">
-          <PhFireSimpleBold />
-        </span>
-        <p>{reactions} Reactions</p>
       </div>
     </div>
   );
