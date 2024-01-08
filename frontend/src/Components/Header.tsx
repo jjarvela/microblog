54dd48290adb70178a7776407153cb5d11e76f0e
Tero Salmela <tero.salmela@reun.eu>
2024-01-08 15:18:55 +0200

    Center header logo in xs

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -2,16 +2,13 @@ import { NavLink } from "react-router-dom";
 import { useBreakpoint } from "../Hooks/BreakpointHook";
 
 function Header() {
-  const { isSm } = useBreakpoint("sm");
   const { isXs } = useBreakpoint("xs");
   return (
     <header className="flex w-full flex-row bg-primary">
-      {/* Extra margin for the sidebar menu button. */}
-      {!isXs && <div className="w-14">&nbsp;</div>}{" "}
-      <h1 className="flex-1 py-2 pl-8 font-heading text-xl font-bold text-white sm:text-2xl mid:py-3 mid:text-3xl">
+      <h1 className="mx-auto py-2 font-heading text-xl font-bold text-white xs:mx-0 xs:pl-8 sm:text-2xl mid:py-3 mid:text-3xl">
         <NavLink to={"/"}>
           <span className="font-light">µB</span>
-          {isSm && " Microblog"}
+          {isXs && " Microblog"}
         </NavLink>
       </h1>
     </header>
