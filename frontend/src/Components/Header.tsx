feb86ab7ea9fd5c14e6accaf65b84f3d00bf6d63
Tero Salmela <tero.salmela@reun.eu>
2024-01-08 13:30:20 +0200

    Add responsive collapsing LeftSidebar

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -3,9 +3,12 @@ import { useBreakpoint } from "../Hooks/BreakpointHook";
 
 function Header() {
   const { isSm } = useBreakpoint("sm");
+  const { isXs } = useBreakpoint("xs");
   return (
-    <header className="w-full flex-shrink">
-      <h1 className="bg-primary py-2 pl-8 font-heading text-xl font-bold text-white sm:text-2xl mid:py-3 mid:text-3xl">
+    <header className="flex w-full flex-row bg-primary">
+      {/* Extra margin for the sidebar menu button. */}
+      {!isXs && <div className="w-14">&nbsp;</div>}{" "}
+      <h1 className="flex-1 py-2 pl-8 font-heading text-xl font-bold text-white sm:text-2xl mid:py-3 mid:text-3xl">
         <NavLink to={"/"}>
           <span className="font-light">µB</span>
           {isSm && " Microblog"}
