49cd3be82e274126dad775e4fcca6144901bdeb9
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-01-09 14:23:06 +0200

    Add thumbnail for conversations

--- frontend/src/Components/Elements/ConversationThumb.tsx
+++ frontend/src/Components/Elements/ConversationThumb.tsx
@@ -1,29 +1,37 @@
+import React from "react";
 import { ProfilePicture } from "./ProfilePicture";
 
 type ConversationThumbProps = {
   recipientName: string;
-  recipient: string;
+  recipientHandle: string;
+  readStatus: boolean;
 };
 
 export default function ConversationThumb({
   recipientName,
-  recipient,
+  recipientHandle,
+  readStatus,
 }: ConversationThumbProps) {
   return (
-    <div className="flex flex-col border-[1px] border-solid border-black50 p-2">
-      <div className="mr-auto flex flex-row flex-wrap items-center gap-4">
-        <div className="mx-auto">
-          <ProfilePicture width={50} />
+    <div className="flex flex-col overflow-hidden border-[1px] border-solid border-black50 p-2">
+      <div className="items-between flex flex-row gap-2">
+        <div className="flex flex-row items-center gap-2">
+          <div className="">
+            <ProfilePicture width={50} />
+          </div>
+          <div className="flex w-[70%] flex-col xl:w-40">
+            <p className="truncate">{recipientName}</p>
+            <small className="text-black50">{recipientHandle}</small>
+          </div>
         </div>
-        <div className={"mx-auto flex flex-col"}>
-          <h5>{recipientName}</h5>
-          <p className="text-black50">{recipient}</p>
+        <div className="flex-grow self-start text-end">
+          <p className="text-xs text-black50">{new Date().toLocaleString()}</p>
         </div>
       </div>
-      <div className="text-ellipsis">
-        <p className="max-w-[80%] text-ellipsis px-1 pt-2">
+      <div className="max-h-[50%] max-w-[80%]">
+        <small className={`px-1 pt-2 ${readStatus && "text-black50"}`}>
           Here we have the latest message in the conversation
-        </p>
+        </small>
       </div>
     </div>
   );
