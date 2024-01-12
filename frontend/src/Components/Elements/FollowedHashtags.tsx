f9ab7f9bbcccce8219f4d22d9c324db16da20693
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-01-12 16:11:24 +0200

    followed hashtags

--- frontend/src/Components/Elements/FollowedHashtags.tsx
+++ frontend/src/Components/Elements/FollowedHashtags.tsx
@@ -1,7 +1,37 @@
+import Button from "./Button";
+
 export default function FollowedHashtags() {
   return (
-    <div className="p-2">
-      <h4>Followed hashtags</h4>
+    <div className="mt-4 flex flex-col gap-2">
+      <div className="flex w-max flex-col self-center border-b-[1px] border-black50 p-2 sm:flex-row sm:gap-5">
+        <p>
+          <a>#{"tag"}</a>
+        </p>
+        <small className="self-center text-black50">New Posts</small>
+        <Button class="btn-primary w-fit">
+          <small>Unfollow</small>
+        </Button>
+      </div>
+
+      <div className="flex w-max flex-col self-center border-b-[1px] border-black50 p-2 sm:flex-row sm:gap-5">
+        <p>
+          <a>#{"tag"}</a>
+        </p>
+        <small className="self-center text-black50">New Posts</small>
+        <Button class="btn-primary w-fit">
+          <small>Unfollow</small>
+        </Button>
+      </div>
+
+      <div className="flex w-max flex-col self-center border-b-[1px] border-black50 p-2 sm:flex-row sm:gap-5">
+        <p>
+          <a>#{"tag"}</a>
+        </p>
+        <small className="self-center text-black50">New Posts</small>
+        <Button class="btn-primary w-fit">
+          <small>Unfollow</small>
+        </Button>
+      </div>
     </div>
   );
 }
