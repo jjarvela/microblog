0fc3a97e5df482bfca2fdc6909ac9754d72cf3ff
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-01-12 13:48:43 +0200

    responsive collapse for messaging elements

--- frontend/src/Components/Elements/ConversationThumb.tsx
+++ frontend/src/Components/Elements/ConversationThumb.tsx
@@ -5,21 +5,43 @@ type ConversationThumbProps = {
   recipientName: string;
   recipientHandle: string;
   readStatus: boolean;
+  setClosed: React.Dispatch<React.SetStateAction<boolean>>;
+  openMessage: {
+    recipientName: string;
+    recipientHandle: string;
+    messages: never[];
+  };
+  setOpenMessage: React.Dispatch<
+    React.SetStateAction<{
+      recipientName: string;
+      recipientHandle: string;
+      messages: never[];
+    }>
+  >;
 };
 
 export default function ConversationThumb({
   recipientName,
   recipientHandle,
   readStatus,
+  openMessage,
+  setOpenMessage,
+  setClosed,
 }: ConversationThumbProps) {
   return (
-    <div className="flex flex-col overflow-hidden border-[1px] border-solid border-black50 p-2">
+    <button
+      className="flex w-full flex-col overflow-hidden border-[1px] border-solid border-black50 p-2"
+      onClick={() => {
+        setOpenMessage({ ...openMessage, recipientName, recipientHandle });
+        setClosed(false);
+      }}
+    >
       <div className="items-between flex flex-row gap-2">
         <div className="flex flex-row items-center gap-2">
           <div className="">
             <ProfilePicture width={50} />
           </div>
-          <div className="flex w-[70%] flex-col xl:w-40">
+          <div className="flex w-20 flex-col sm:w-60 xl:w-40">
             <p className="truncate">{recipientName}</p>
             <small className="text-black50">{recipientHandle}</small>
           </div>
@@ -29,10 +51,12 @@ export default function ConversationThumb({
         </div>
       </div>
       <div className="max-h-[50%] max-w-[80%]">
-        <small className={`px-1 pt-2 ${readStatus && "text-black50"}`}>
+        <small
+          className={`line-clamp-2 px-1 pt-2 ${readStatus && "text-black50"}`}
+        >
           Here we have the latest message in the conversation
         </small>
       </div>
-    </div>
+    </button>
   );
 }
--- frontend/src/Components/UserMessages.tsx
+++ frontend/src/Components/UserMessages.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ConversationThumb from "./Elements/ConversationThumb";
 import TextAreaInput from "./Elements/TextAreaInput";
 import Conversation from "./Elements/Conversation";
@@ -6,40 +6,82 @@ import MaterialSymbolsSentimentSatisfiedOutline from "./Icons/MaterialSymbolsSen
 import MaterialSymbolsAddPhotoAlternateOutlineRounded from "./Icons/MaterialSymbolsAddPhotoAlternateOutlineRounded";
 import MaterialSymbolsSendRounded from "./Icons/MaterialSymbolsSendRounded";
 import Button from "./Elements/Button.tsx";
+import { useBreakpoint } from "../Hooks/BreakpointHook.tsx";
 
 const UserMessages = () => {
+  const [openMessage, setOpenMessage] = useState({
+    recipientName: "",
+    recipientHandle: "",
+    messages: [],
+  });
+
+  const { isXl } = useBreakpoint("xl");
+  const [closed, setClosed] = useState(true);
+
   return (
     <div className="mx-1 h-[94%]">
       <h2>This is the user's messaging hub</h2>
-      <div className="flex h-full w-full  flex-col xl:flex-row">
-        <div className="scrollbar-thin w-full border-b-[1px] border-solid border-black50 xl:w-[50%] xl:overflow-y-scroll xl:border-b-0 xl:border-r-[1px]">
+      <div className="h-full w-full xl:flex xl:flex-row">
+        <div
+          className={`${
+            !isXl && !closed && "collapse h-0"
+          } scrollbar-thin w-full border-b-[1px] border-solid border-black50 xl:w-[50%] xl:overflow-y-scroll xl:border-b-0 xl:border-r-[1px]`}
+        >
           <ConversationThumb
             recipientName="Test User ✨"
             recipientHandle="@testuser"
             readStatus={true}
+            setClosed={setClosed}
+            openMessage={openMessage}
+            setOpenMessage={setOpenMessage}
           />
           <ConversationThumb
             recipientName="Fancy User"
             recipientHandle="@fancyuser"
             readStatus={false}
+            setClosed={setClosed}
+            openMessage={openMessage}
+            setOpenMessage={setOpenMessage}
           />
           <ConversationThumb
             recipientName="Dickerson"
             recipientHandle="@dickerson99"
             readStatus={true}
+            setClosed={setClosed}
+            openMessage={openMessage}
+            setOpenMessage={setOpenMessage}
           />
           <ConversationThumb
             recipientName="Spammer McSpamface"
             recipientHandle="@spamlord"
             readStatus={true}
+            setClosed={setClosed}
+            openMessage={openMessage}
+            setOpenMessage={setOpenMessage}
           />
         </div>
-        <div className="flex flex-grow flex-col">
-          <Conversation />
+        <div
+          className={`${
+            !isXl && closed && "collapse"
+          } relative flex h-full flex-grow flex-col`}
+        >
+          {!closed ? (
+            <Conversation
+              recipientName={openMessage.recipientName}
+              recipientHandle={openMessage.recipientHandle}
+              setClosed={setClosed}
+            />
+          ) : (
+            <h4 className="p-4 text-center">Start a new conversation</h4>
+          )}
           <div className="flex flex-row justify-center gap-2 border-t-[1px] border-solid border-black50 p-2 text-center">
             <span className="px-auto flex flex-row justify-center self-center text-xl text-black50">
-              <MaterialSymbolsSentimentSatisfiedOutline />
-              <MaterialSymbolsAddPhotoAlternateOutlineRounded />
+              <button>
+                <MaterialSymbolsSentimentSatisfiedOutline />
+              </button>
+              <button>
+                <MaterialSymbolsAddPhotoAlternateOutlineRounded />
+              </button>
             </span>
             <span className="flex-grow">
               <TextAreaInput class="w-full" />
