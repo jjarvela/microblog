fea31ef5f962bad30b72363a688d7f53a976a507
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-01-18 13:03:11 +0200

    add-notifications-page

---
+++ frontend/src/Components/Elements/UserNotificationBox.tsx
@@ -0,0 +1,71 @@
+import { ProfilePicture } from "../Elements/ProfilePicture";
+
+type UserNotificationBoxProps = {
+  userName: string;
+  groupName: string;
+  text: string;
+  notifications: string[];
+};
+
+const UserNotificationBox = ({
+  userName,
+  groupName,
+  notifications,
+}: UserNotificationBoxProps) => {
+  const renderNotifications = () => {
+    if (notifications.length >= 3) {
+      return (
+        <div className="relative z-30 mb-5 h-40 w-5/6 rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 drop-shadow-lg dark:from-gray-500 dark:to-gray-700">
+          <div className="absolute -bottom-3 z-20 h-40 w-full rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 drop-shadow-lg  dark:from-gray-500 dark:to-gray-700"></div>
+          <div className="absolute -bottom-6 z-40 flex h-40 w-full rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 p-3 drop-shadow-lg  dark:from-gray-500 dark:to-gray-700">
+            <div className="mx-3 flex flex-col gap-3">
+              <ProfilePicture width={70} />
+            </div>
+            <div className="flex flex-col">
+              <h5>{userName}</h5>
+              <p>I really like you. Please reply to my messages!!!1</p>
+              <p className="mt-8">69 more notifications</p>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (notifications.length === 2) {
+      return (
+        <div className="relative mb-3 h-40 w-5/6 rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300  drop-shadow-lg dark:from-gray-500 dark:to-gray-700">
+          <div className="absolute -bottom-3 flex h-40 w-full rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 p-3  drop-shadow-lg dark:from-gray-500 dark:to-gray-700">
+            <div className="mx-3 flex flex-col gap-3">
+              <ProfilePicture width={70} />
+            </div>
+            <div className="flex flex-col">
+              <h5>{userName}</h5>
+              <p>Simaresepti</p>
+              <p className="mt-8">1 more notification</p>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (notifications.length === 1) {
+      return (
+        <div className="relative flex h-40 w-5/6 flex-row rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 p-3 drop-shadow-lg dark:from-gray-500 dark:to-gray-700">
+          <div className="mx-3 flex flex-col gap-3">
+            <ProfilePicture width={70} />
+          </div>
+          <div className="flex flex-col">
+            <h5>{groupName}</h5>
+            <p>Jarkon Porkkanakakkukatastrofi</p>
+            <p className="mt-8">1 more notification</p>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="my-6 flex flex-col items-center gap-10">
+      {renderNotifications()}
+    </div>
+  );
+};
+
+export default UserNotificationBox;
--- frontend/src/Components/UserNotifications.tsx
+++ frontend/src/Components/UserNotifications.tsx
@@ -1,7 +1,31 @@
+import UserNotificationBox from "./Elements/UserNotificationBox";
+
 const UserNotifications = () => {
-    return(
-        <h2>This is the user's notifications hub</h2>
-    );
+  return (
+    <div>
+      <h2 className="m-3 text-center">This is the user's notifications hub</h2>
+      <div>
+        <UserNotificationBox
+          notifications={["noti1"]}
+          groupName={"BuuttiBois"}
+          userName={""}
+          text={""}
+        />
+        <UserNotificationBox
+          notifications={["noti1", "noti2"]}
+          userName="Mutsis"
+          groupName={""}
+          text={""}
+        />
+        <UserNotificationBox
+          notifications={["noti1", "noti2", "noti3"]}
+          userName="dickerson99"
+          groupName={""}
+          text={""}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default UserNotifications;
