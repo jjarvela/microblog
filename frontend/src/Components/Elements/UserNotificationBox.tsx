3b020a4cac62401c2062c24edfbeb3aabb84ce8a
Sami Mäkelä <141029876+TerlfY@users.noreply.github.com>
2024-01-19 13:39:18 +0200

    Update frontend/src/Components/Elements/UserNotificationBox.tsx
    Co-authored-by: OneginIII <37181529+OneginIII@users.noreply.github.com>

--- frontend/src/Components/Elements/UserNotificationBox.tsx
+++ frontend/src/Components/Elements/UserNotificationBox.tsx
@@ -16,7 +16,7 @@ const UserNotificationBox = ({
   const renderNotifications = () => {
     if (notifications.length >= 3) {
       return (
-        <div className="relative z-30 mb-5 h-40 w-5/6 rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 drop-shadow-lg dark:from-gray-500 dark:to-gray-700">
+        <div className="relative z-30 mb-5 h-40 w-5/6 rounded-md border border-primary bg-gradient-to-r from-white to-black25 drop-shadow-lg dark:from-black75 dark:to-black">
           <div className="absolute -bottom-3 z-20 h-40 w-full rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 drop-shadow-lg  dark:from-gray-500 dark:to-gray-700"></div>
           <div className="absolute -bottom-6 z-40 flex h-40 w-full rounded-md border border-orange-500 bg-gradient-to-r from-gray-200 to-gray-300 p-3 drop-shadow-lg  dark:from-gray-500 dark:to-gray-700">
             <div className="mx-3 flex flex-col gap-3">
