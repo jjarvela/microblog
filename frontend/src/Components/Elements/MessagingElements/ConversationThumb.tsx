855f92e5ae1c72a75d4af969639c6d496a8a5217
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-01-25 13:03:20 +0200

    Add context menu for conversations

--- frontend/src/Components/Elements/MessagingElements/ConversationThumb.tsx
+++ frontend/src/Components/Elements/MessagingElements/ConversationThumb.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { ProfilePicture } from "../ProfilePicture";
+import MaterialSymbolsCloseSmallRounded from "../../Icons/MaterialSymbolsCloseSmallRounded";
+import MaterialSymbolsMarkEmailUnreadRounded from "../../Icons/MaterialSymbolsMarkEmailUnreadRounded";
+import MaterialSymbolsMarkEmailReadRounded from "../../Icons/MaterialSymbolsMarkEmailReadRounded";
+import MaterialSymbolsBlock from "../../Icons/MaterialSymbolsBlock";
+import MaterialSymbolsDeleteForeverOutlineRounded from "../../Icons/MaterialSymbolsDeleteForeverOutlineRounded";
 
 type ConversationThumbProps = {
   recipientName: string;
@@ -28,12 +33,38 @@ export default function ConversationThumb({
   setOpenMessage,
   setClosed,
 }: ConversationThumbProps) {
+  const [showContextMenu, setShowContextMenu] = useState(false);
+
+  const useClickOutside = (callback: () => void) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const handleClick = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node))
+          callback();
+      };
+
+      document.addEventListener("click", handleClick, true);
+
+      return () => document.removeEventListener("click", handleClick, true);
+    }, [ref]);
+
+    return ref;
+  };
+
+  const ref = useClickOutside(() => setShowContextMenu(false));
+
   return (
     <button
-      className="flex w-full flex-col overflow-hidden border-[1px] border-solid border-black50 p-2"
+      className=" relative flex w-full flex-col overflow-hidden border-[1px] border-solid border-black50 p-2"
       onClick={() => {
-        setOpenMessage({ ...openMessage, recipientName, recipientHandle });
-        setClosed(false);
+        !showContextMenu &&
+          setOpenMessage({ ...openMessage, recipientName, recipientHandle });
+        !showContextMenu && setClosed(false);
+      }}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setShowContextMenu(true);
       }}
     >
       <div className="items-between flex w-full flex-row gap-2">
@@ -59,6 +90,45 @@ export default function ConversationThumb({
           Here we have the latest message in the conversation
         </small>
       </div>
+      {showContextMenu && (
+        <>
+          <div
+            className="absolute right-0 top-0 z-30"
+            onClick={() => setShowContextMenu(false)}
+          >
+            <MaterialSymbolsCloseSmallRounded className="text-lg" />
+          </div>
+          <div
+            ref={ref}
+            className="absolute right-0 top-2 z-20 flex flex-col justify-center rounded-xl border border-black50 bg-white p-2 dark:border-white50 dark:bg-black dark:text-white"
+          >
+            <a
+              className="flex gap-2  border-b-[1px] border-b-black50 py-1 text-xs"
+              onClick={() => setShowContextMenu(false)}
+            >
+              {readStatus ? (
+                <MaterialSymbolsMarkEmailUnreadRounded />
+              ) : (
+                <MaterialSymbolsMarkEmailReadRounded />
+              )}{" "}
+              <span>Mark as {readStatus ? "unread" : "read"}</span>
+            </a>
+            <a
+              className="flex gap-2 border-b-[1px] border-b-black50 py-1 text-xs"
+              onClick={() => setShowContextMenu(false)}
+            >
+              <MaterialSymbolsDeleteForeverOutlineRounded />
+              <span>Delete</span>
+            </a>
+            <a
+              className="flex gap-2 border-b-[1px] border-b-black50 py-1 text-xs"
+              onClick={() => setShowContextMenu(false)}
+            >
+              <MaterialSymbolsBlock /> <span>Block</span>
+            </a>
+          </div>
+        </>
+      )}
     </button>
   );
 }
