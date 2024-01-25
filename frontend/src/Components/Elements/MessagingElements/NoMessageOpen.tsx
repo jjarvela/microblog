8a6fe7399002e0d73b20a3e2d44d3d8d5fb1b273
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-01-25 15:46:22 +0200

    Add conditional rendering to back arrow based on breakpoint

--- frontend/src/Components/Elements/MessagingElements/NoMessageOpen.tsx
+++ frontend/src/Components/Elements/MessagingElements/NoMessageOpen.tsx
@@ -3,21 +3,25 @@ import MaterialSymbolsChatAddOnRounded from "../../Icons/MaterialSymbolsChatAddO
 import Button from "../Button.tsx";
 import { MaterialSymbolsChevronLeftRounded } from "../../Icons/MaterialSymbolsChevronLeftRounded";
 import { Link } from "react-router-dom";
+import { useBreakpoint } from "../../../Hooks/BreakpointHook.tsx";
 
 type NoMessageOpenProps = {
   setClosed: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export default function NoMessageOpen({ setClosed }: NoMessageOpenProps) {
+  const { isXl } = useBreakpoint("xl");
   return (
     <div className=" flex h-[70%] flex-col">
-      <Link
-        to={"/messages"}
-        className="text-3xl text-black50"
-        onClick={() => setClosed(true)}
-      >
-        <MaterialSymbolsChevronLeftRounded />
-      </Link>
+      {!isXl && (
+        <Link
+          to={"/messages"}
+          className="text-3xl text-black50"
+          onClick={() => setClosed(true)}
+        >
+          <MaterialSymbolsChevronLeftRounded />
+        </Link>
+      )}
       <h4 className="p-4 text-center">Start a new conversation</h4>
       <div className="flex justify-center gap-2">
         <TextInput placeholder="Search for users..." />
