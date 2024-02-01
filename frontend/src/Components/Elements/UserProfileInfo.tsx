48d43bc908755e128420dc3467f07a324042a44e
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-01 09:32:25 +0200

    Add prop to disable popup

--- frontend/src/Components/Elements/UserProfileInfo.tsx
+++ frontend/src/Components/Elements/UserProfileInfo.tsx
@@ -10,6 +10,7 @@ type UserProfileInfoProps = {
   class?: string;
   nameClass?: string;
   handleClass?: string;
+  disablePopup?: boolean;
 };
 
 function UserProfileInfo({
@@ -18,6 +19,7 @@ function UserProfileInfo({
   class: classAdd,
   nameClass,
   handleClass,
+  disablePopup,
 }: UserProfileInfoProps) {
   const self = useUser().user;
   const [showPopup, setShowPopup] = useState(false);
@@ -49,22 +51,24 @@ function UserProfileInfo({
             onMouseEnter={() => {
               /*Get the window height and current x and y positions of the popup to adjust its offset so it's always fully on screen*/
               if (
+                !disablePopup &&
                 window.innerHeight - popup.current!.getBoundingClientRect().y <
-                400
+                  400
               ) {
                 setFloatY("bottom-[5rem]");
               } else {
                 setFloatY("top-0");
               }
               if (
+                !disablePopup &&
                 window.innerWidth - popup.current!.getBoundingClientRect().x >
-                400
+                  400
               ) {
                 setFloatX("left-0");
               } else {
                 setFloatX("left-4");
               }
-              setShowPopup(true);
+              !disablePopup && setShowPopup(true);
             }}
           >
             {user.screenName}
@@ -73,38 +77,40 @@ function UserProfileInfo({
         </div>
       </Link>
 
-      <div
-        ref={popup}
-        className={`${
-          !showPopup ? "z-[9999] h-0 w-0" : "h-[max-content] w-[max-content]"
-        } absolute ${floatX} ${floatY} overflow-hidden `}
-        onMouseLeave={() => setShowPopup(false)}
-      >
-        <div className="mt-[3rem] rounded-xl border border-black50 bg-white dark:border-white50 dark:bg-black dark:text-white">
-          <div className="flex flex-col gap-2 p-4">
-            <div className="flex flex-row justify-between">
-              <ProfilePicture image={user.profileImage} width={60} />
-              {self?.userName !== user.userName && (
-                <Button className="btn-primary">
-                  <p>Follow</p>
-                </Button>
-              )}
-            </div>
-            <Link to={"/user/" + user.userName} className="flex flex-col">
-              <h5 className="hover:underline hover:underline-offset-2 ">
-                {user.screenName}
-              </h5>
-              <small>{user.userName}</small>
-            </Link>
-            <p>Fetch user about at some point</p>
-            {/*Add proper follower counts once we get them*/}
-            <div className="bold flex flex-row gap-2 text-lg text-secondary">
-              <p>{3} Followers</p>
-              <p>{5} Following</p>
+      {!disablePopup && (
+        <div
+          ref={popup}
+          className={`${
+            !showPopup ? "z-[9999] h-0 w-0" : "h-[max-content] w-[max-content]"
+          } absolute ${floatX} ${floatY} overflow-hidden `}
+          onMouseLeave={() => setShowPopup(false)}
+        >
+          <div className="mt-[3rem] rounded-xl border border-black50 bg-white dark:border-white50 dark:bg-black dark:text-white">
+            <div className="flex flex-col gap-2 p-4">
+              <div className="flex flex-row justify-between">
+                <ProfilePicture image={user.profileImage} width={60} />
+                {self?.userName !== user.userName && (
+                  <Button className="btn-primary">
+                    <p>Follow</p>
+                  </Button>
+                )}
+              </div>
+              <Link to={"/user/" + user.userName} className="flex flex-col">
+                <h5 className="hover:underline hover:underline-offset-2 ">
+                  {user.screenName}
+                </h5>
+                <small>{user.userName}</small>
+              </Link>
+              <p>Fetch user about at some point</p>
+              {/*Add proper follower counts once we get them*/}
+              <div className="bold flex flex-row gap-2 text-lg text-secondary">
+                <p>{3} Followers</p>
+                <p>{5} Following</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
