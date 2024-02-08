3d2399382578160c401e0db7faee33af0945bfde
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-02-08 15:47:25 +0200

    Add withCredentials

--- frontend/src/Services/conversationService.ts
+++ frontend/src/Services/conversationService.ts
@@ -2,28 +2,36 @@ import axios from "axios";
 import { serverUrl } from "../globalData";
 
 function getUserConversations(userId: string) {
-  return axios.get(`${serverUrl}/user/${userId}/conversations`).then((res) => {
-    console.log(res.data);
-    return res.data;
-  });
+  return axios
+    .get(`${serverUrl}/user/${userId}/conversations`, { withCredentials: true })
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    });
 }
 
 function createConversation(newConversation: NewConversation) {
   return axios
-    .post(`${serverUrl}/conversation/`, newConversation)
+    .post(`${serverUrl}/conversation/`, newConversation, {
+      withCredentials: true,
+    })
     .then((res) => res.data);
 }
 
 function getConversation(conversationId: number) {
   return axios
-    .get(`${serverUrl}/conversation/${conversationId}`)
+    .get(`${serverUrl}/conversation/${conversationId}`, {
+      withCredentials: true,
+    })
     .then((res) => res.data);
 }
 
 function getMessages(conversationId: number) {
   console.log("get messages request");
   return axios
-    .get(`${serverUrl}/conversation/${conversationId}/messages`)
+    .get(`${serverUrl}/conversation/${conversationId}/messages`, {
+      withCredentials: true,
+    })
     .then((res) => res.data);
 }
 
@@ -32,22 +40,30 @@ function addNewMessage(
   message: NewConversationMessage,
 ) {
   return axios
-    .post(`${serverUrl}/conversation/${conversationId}`, message)
+    .post(`${serverUrl}/conversation/${conversationId}`, message, {
+      withCredentials: true,
+    })
     .then((res) => res.data);
 }
 
 function editMessage(conversationId: number, messageId: number) {
-  return axios.put(`${serverUrl}/conversation/${conversationId}/${messageId}`);
+  return axios.put(`${serverUrl}/conversation/${conversationId}/${messageId}`, {
+    withCredentials: true,
+  });
 }
 
 function deleteConversation(conversationId: number) {
   return axios
-    .delete(`${serverUrl}/conversation/${conversationId}/`)
+    .delete(`${serverUrl}/conversation/${conversationId}/`, {
+      withCredentials: true,
+    })
     .then((res) => res.data);
 }
 
 function deleteMessage(conversationId: number) {
-  return axios.delete(`${serverUrl}/conversation/${conversationId}`);
+  return axios.delete(`${serverUrl}/conversation/${conversationId}`, {
+    withCredentials: true,
+  });
 }
 
 export default {
