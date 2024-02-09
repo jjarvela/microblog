3d7f8ad9f4dcce83710b5a8902b7c9f25e1e5f69
Tero Salmela <tero.salmela@reun.eu>
2024-02-09 09:07:52 +0200

    Initial request credentials support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,11 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import UserWrapper from "./UserWrapper.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 
 export const queryClient = new QueryClient();
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
