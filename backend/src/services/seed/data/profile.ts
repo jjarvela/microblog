145197a1b42dae5a7877216c293bd28cae788391
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-12 11:49:29 +0200

    Update seed data with screen names, profile images and John's profile

--- backend/src/services/seed/data/profile.ts
+++ backend/src/services/seed/data/profile.ts
@@ -1,10 +1,10 @@
-import { userid1 } from "./users";
+import { userid1, userid2 } from "./users";
 
 export const elementTypes = [
   { id: 0, name: "text" },
   { id: 1, name: "links" },
   { id: 2, name: "media" },
-  { id: 3, name: "post" },
+  { id: 3, name: "post" }
 ];
 
 export const userProfiles = [
@@ -12,6 +12,13 @@ export const userProfiles = [
     user_id: userid1,
     profile_text: "Hello! My name is Jane.",
     header_media_id: 0,
-    homepage: "microblog.com",
+    homepage: "microblog.com"
   },
+
+  {
+    user_id: userid2,
+    profile_text: "Johnny Boy's in town",
+    header_media_id: 0,
+    homepage: "microblog.com"
+  }
 ];
