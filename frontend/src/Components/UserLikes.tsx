c6cd7b49606e07ce845831ee548fbb62a9744458
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-16 16:54:10 +0200

    Comment out like fetching for now

--- frontend/src/Components/UserLikes.tsx
+++ frontend/src/Components/UserLikes.tsx
@@ -1,4 +1,4 @@
-import Post from "./Elements/PostElements/Post";
+//import Post from "./Elements/PostElements/Post";
 import { ProfileContext } from "./UserPage";
 import { useContext } from "react";
 
@@ -10,22 +10,10 @@ function UserLikes() {
       <h2 className="my-4 text-center">
         {profile.details?.screenName}'s Likes
       </h2>
-      <Post
-        post={{
-          postOwner: {
-            userName: "@madasitgets",
-            screenName: "Outraged user 951 😤",
-            followers: [],
-            following: [],
-          },
-          text: "I'm saying something very controversial! You should definitely not share this anywhere online, but we can't understand that because technology melted our collective brains.",
-          reactions: 99999,
-          tags: ["AngryAtEverything", "ExceptWhatIsActuallyWrong"],
-          media: [],
-          time: new Date(),
-        }}
+      {/*<Post
+        post={}
         topInfo={profile.details?.userName + " liked this"}
-      />
+  />*/}
     </div>
   );
 }
