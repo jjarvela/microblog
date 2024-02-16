d96dbb0405c5bf92bee23e200ffed5f00cbb375f
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-16 16:43:51 +0200

    Add additional users and posts to seed data

--- backend/src/services/seed/data/profile.ts
+++ backend/src/services/seed/data/profile.ts
@@ -1,4 +1,4 @@
-import { userid1, userid2 } from "./users";
+import { userid1, userid2, userid3 } from "./users";
 
 export const elementTypes = [
   { id: 0, name: "text" },
@@ -20,5 +20,27 @@ export const userProfiles = [
     profile_text: "Johnny Boy's in town",
     header_media_id: 0,
     homepage: "microblog.com"
+  },
+
+  {
+    user_id: userid3,
+    profile_text: "I am new here, please be my friend",
+    header_media_id: 0,
+    homepage: "microblog.com"
+  },
+
+  {
+    user_id: "3fa8d704-2c84-45e9-9451-cb0673de810f",
+    profile_text: "Oooooo so fancy",
+    header_media_id: 0,
+    homepage: null
+  },
+
+  {
+    id: "eee98cf6-6472-41eb-86c6-39445ba77f49",
+    user_id: "5454dc94-2adf-450b-8f45-ece44f1284d9",
+    profile_text: "ONLY I KNOW THE TRUTH THE REST OF YOU ARE SHEEP",
+    header_media_id: null,
+    homepage: null
   }
 ];
