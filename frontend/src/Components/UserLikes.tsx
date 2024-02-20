222aa6f9a4a5bf2afb7c2449742f99ec176fce71
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-02-20 10:27:04 +0200

    Fetch user likes on the user profile

--- frontend/src/Components/UserLikes.tsx
+++ frontend/src/Components/UserLikes.tsx
@@ -1,19 +1,32 @@
-//import Post from "./Elements/PostElements/Post";
+import Post from "./Elements/PostElements/Post";
+import { useQuery } from "@tanstack/react-query";
 import { ProfileContext } from "./UserPage";
 import { useContext } from "react";
+import postService from "../Services/postService";
 
 function UserLikes() {
   const profile = useContext(ProfileContext);
+
+  const userLikeQuery = useQuery({
+    queryKey: ["likes", profile.details?.userName],
+    queryFn: () => {
+      return postService.getUserReactions(profile.details?.id || "", ["like"]);
+    },
+    enabled: !!profile.details,
+  });
 
   return (
     <div className="my-4">
       <h2 className="my-4 text-center">
         {profile.details?.screenName}'s Likes
       </h2>
-      {/*<Post
-        post={}
-        topInfo={profile.details?.userName + " liked this"}
-  />*/}
+      {userLikeQuery.data &&
+        (userLikeQuery.data as BlogPostFromServer[]).map((post) => (
+          <Post
+            post={post}
+            topInfo={profile.details?.userName + " liked this"}
+          />
+        ))}
     </div>
   );
 }
